7243c3a8d51410cf43a8bce40a2e5734fc9d3326
Ilya Buhlakou <ilya_buhlakou@epam.com>
2023-03-06 11:15:21 +0300

    feat: initial commit

---
+++ src/test/java/com/epam/ilyabuhlakou/springfoundation/SpringFoundationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.ilyabuhlakou.springfoundation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoundationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
